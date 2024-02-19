06d17af8f67dfad515eeefb9d51146c91a1ec660
Vadzim <shylkovad@gmail.com>
2024-02-19 21:36:06 +0300

    sortingName and sortingDistrictName

--- src/main/java/ru/sberbank/City.java
+++ src/main/java/ru/sberbank/City.java
@@ -17,6 +17,54 @@ public class City {
         this.foundation = foundation;
     }
 
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getFoundation() {
+        return foundation;
+    }
+
+    public void setFoundation(String foundation) {
+        this.foundation = foundation;
+    }
+
     @Override
     public String toString() {
         return "City{" +
--- src/main/java/ru/sberbank/Main.java
+++ src/main/java/ru/sberbank/Main.java
@@ -3,6 +3,8 @@ package ru.sberbank;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,7 +14,9 @@ public class Main {
         // считывание файла
         String fileName = "ВС_Java.csv";
 
-        List<City> cities = readCitiesFromFile(fileName);
+//        List<City> cities = readCitiesFromFile(fileName); // вывод списка
+//        List<City> cities = sortingName(fileName); // сортировка по имени
+        List<City> cities = sortingDistrictName(fileName); // сортировка по федеральному округу и имени
 
         // Выводим полученные объекты City в консоль
         for (City city : cities) {
@@ -31,6 +35,8 @@ public class Main {
                 String input = scanner.nextLine();
                 String[] cityData = input.split(";");
 
+//                cities.add(cityData);
+
                 if (cityData.length == 6) {
                     City city = new City(
                             Integer.parseInt(cityData[0]),
@@ -47,6 +53,21 @@ public class Main {
             System.out.println("Файл не найден");
 //            e.printStackTrace();
         }
+
+        return cities;
+    }
+
+    // сортировка по имени
+    public static List<City> sortingName(String fileName) {
+        List<City> cities = readCitiesFromFile(fileName);
+        Collections.sort(cities, Comparator.comparing(City::getName));
+        return cities;
+    }
+
+    // сортировка по федеральному округу и имени
+    public static List<City> sortingDistrictName(String fileName) {
+        List<City> cities = readCitiesFromFile(fileName);
+        Collections.sort(cities, Comparator.comparing(City::getDistrict).thenComparing(City::getName));
         return cities;
     }
 }
